4ffdbd0eb557fb7fa6896c1bd26d9ed049297a9e
readelobdill <reade@lobdill.com>
2016-10-04 10:02:14 -0700

    implement sass-loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import * as _ from "lodash";
-//TODO: sort arrows, preview, style + sass
+//TODO: sort arrows, preview, style
 @Component({
 	selector: 'my-app',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 	private values: Object[];
