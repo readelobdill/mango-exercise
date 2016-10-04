711a16e515a60f1d183db774adc2e124cc50208e
readelobdill <reade@lobdill.com>
2016-10-04 11:09:12 -0700

    fix indentation and update comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,42 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import * as _ from "lodash";
-//TODO: sort arrows, preview, style
+//TODO: song preview
 @Component({
-	selector: 'my-app',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	private values: Object[];
-	private filterQuery: string = '';
-	private isReverse: boolean = true;
-	private searchQuery: string = 'mango';
-	private sortKey: string = 'popularity';
+    private values: Object[];
+    private filterQuery: string = '';
+    private isReverse: boolean = true;
+    private searchQuery: string = 'man go';
+    private sortKey: string = 'popularity';
 
-	constructor(private http: Http){
-		this.getValues();
-		this.getValues = _.debounce(this.getValues, 250);
-	}
-	getValues(){
-		if(this.searchQuery){
-			let url = `https://api.spotify.com/v1/search?type=track&q=${this.searchQuery}`;
-			this.http.get(url).map((response) => response.json()).subscribe(result => {
-				this.values = result.tracks.items;
-			});
-		}
-	}
-	getReadableDuration(trackMs: number){
-		let min = Math.floor(trackMs / 60000);
-  		let sec = `0${Math.floor((trackMs % 60000) / 1000)}`.slice(-2);
-		return `${min}:${sec}`;
-	}
-	sortBy(sortKey: string){
-		if(!this.isReverse && this.sortKey === sortKey){
-			this.isReverse = true;
-		} else {
-			this.isReverse = false;
-			this.sortKey = sortKey;
-		}
-	}
+    constructor(private http: Http){
+        this.getValues();
+        this.getValues = _.debounce(this.getValues, 250);
+    }
+    getValues(){
+        if(this.searchQuery){
+            let url = `https://api.spotify.com/v1/search?type=track&q=${this.searchQuery}`;
+            this.http.get(url).map((response) => response.json()).subscribe(result => {
+                this.values = result.tracks.items;
+            });
+        }
+    }
+    getReadableDuration(trackMs: number){
+        let min = Math.floor(trackMs / 60000);
+        let sec = `0${Math.floor((trackMs % 60000) / 1000)}`.slice(-2);
+        return `${min}:${sec}`;
+    }
+    sortBy(sortKey: string){
+        if(!this.isReverse && this.sortKey === sortKey){
+            this.isReverse = true;
+        } else {
+            this.isReverse = false;
+            this.sortKey = sortKey;
+        }
+    }
 }
--- src/app/pipes.ts
+++ src/app/pipes.ts
@@ -4,16 +4,17 @@ import * as _ from "lodash";
 //TODO: add fallback sorting states
 @Pipe({name: 'orderTracksBy'})
 export class OrderTracksByPipe implements PipeTransform {
-	transform(array: any[], sortKey: string, reverse: boolean): any[] {
-		let sortedArray = _.sortBy(array, value => _.get(value, sortKey));
-		return reverse ? sortedArray.reverse() : sortedArray;
-	}
+    transform(array: any[], sortKey: string, reverse: boolean): any[] {
+        let sortedArray = _.sortBy(array, value => _.get(value, sortKey));
+        return reverse ? sortedArray.reverse() : sortedArray;
+    }
 }
 
+//TODO: add "no matches" state
 @Pipe({name: 'filterTracks'})
 export class FilterTracksPipe implements PipeTransform {
-	transform(array: any[], query: string): any[] {
-		let regex = new RegExp(query, "i");
-		return array.filter(value => regex.test(value.name) || regex.test(value.artists[0].name));
-	}
+    transform(array: any[], query: string): any[] {
+        let regex = new RegExp(query, "i");
+        return array.filter(value => regex.test(value.name) || regex.test(value.artists[0].name));
+    }
 }
